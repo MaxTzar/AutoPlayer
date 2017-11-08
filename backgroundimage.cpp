3fb9da71ba10f7a4f927a0c4e9401c9048497e9c
Max <maxtzar@hotmail.com>
2017-11-08 18:20:14 +0100

    [Enh] improve the pattern founder

--- backgroundimage.cpp
+++ backgroundimage.cpp
@@ -16,12 +16,14 @@ BackgroundImage::~BackgroundImage()
 
 BackgroundImage::box BackgroundImage::findSampleImage(const QImage &image, int threshold)
 {
-    for( int i = 0; i < this->height(); ++i)
-        for( int j = 0; j < this->width(); ++j)
+    int i = 0;
+    int j = 0;
+    for(  i = 0; i < this->height() - image.height(); ++i)
+        for(  j = 0; j < this->width() - image.width(); ++j)
         {
             bool rslt = findSampleImage(image, j, i, threshold);
             if (rslt)
-                return box(i, j, i + image.width(), j +image.height());
+                return box(i, j, i + image.height(), j +image.width());
         }
     return box();
 }
@@ -30,9 +32,12 @@ BackgroundImage::box BackgroundImage::findSampleImage(const QImage &image, int t
 
 bool BackgroundImage::findSampleImage(const QImage & image, int row, int column, int threshold)
 {
-    for (int i = 0; i < image.height(); ++i)
+    float error = 0.1;
+    int ctR = 0;
+    int ctF = 0;
+    for (int i = 0; i < image.height(); i += 2)
     {
-        for (int j = 0; j < image.width(); ++j)
+        for (int j = 0; j < image.width() - i; j += 2)
         {
             QRgb imagePixelColor = image.pixel(j, i);
             QRgb backgroundColor = this->pixel(row + j, column + i);
@@ -42,6 +47,13 @@ bool BackgroundImage::findSampleImage(const QImage & image, int row, int column,
             int bDelta = qAbs(qBlue(imagePixelColor) - qBlue(backgroundColor));
 
             if ((rDelta > threshold) || (gDelta > threshold) || (bDelta > threshold))
+                ++ctF;
+            else
+                ++ctR;
+            if(ctR == 0)
+                return false;
+            float errorComp = (float(ctF)/float(ctR));
+            if ( errorComp > error)
                 return false;
         }
     }
