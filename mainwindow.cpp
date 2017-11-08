f896ac332e9a36649cfeed20f55aa126a1024e73
Max <maxtzar@hotmail.com>
2017-11-08 18:26:04 +0100

    first test

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QTimer>
 #include <QPixmap>
 #include <QColor>
+#include <QQuickWidget>
+#include <QLayout>
 #include <backgroundimage.h>
 
 static int waitMilliSec = 2000;
@@ -12,6 +14,48 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QQuickWidget *view = new QQuickWidget;
+    view->setSource(QUrl::fromLocalFile("myqmlfile.qml"));
+    view->show();
+
+    QWidget *widget = ui->widget;
+    QLayout *layout = widget->layout();
+    layout->addWidget(view);
+
+
+
+    GraphManager::Elt* elt1 = new GraphManager::Elt();
+    elt1->_name = "elt1";
+    elt1->_pattern = QImage("D:/important/factures/tmp/test.png");
+    GraphManager::Elt* elt2 = new GraphManager::Elt();
+    elt2->_name = "elt2";
+    elt2->_pattern = QImage("D:/important/factures/tmp/elt2.png");
+    GraphManager::Elt* elt3 = new GraphManager::Elt();
+    elt3->_name = "elt3";
+    elt3->_pattern = QImage("D:/important/factures/tmp/test3.png");
+    GraphManager::Elt* elt4 = new GraphManager::Elt();
+    elt4->_name = "elt4";
+    elt4->_pattern = QImage("D:/important/factures/tmp/test4.png");
+    GraphManager::Elt* elt5 = new GraphManager::Elt();
+    elt5->_name = "elt5";
+    elt5->_pattern = QImage("D:/important/factures/tmp/test5.png");
+
+    graphMger.addElts(elt1);
+    graphMger.addElts(elt2);
+    graphMger.addElts(elt3);
+    graphMger.addElts(elt4);
+    graphMger.addElts(elt5);
+
+    graphMger.connectTo(elt1, elt2);
+    graphMger.connectTo(elt1, elt3);
+    graphMger.connectTo(elt3, elt4);
+    graphMger.connectTo(elt3, elt5);
+    graphMger.connectTo(elt2, elt1);
+    graphMger.connectTo(elt4, elt1);
+    graphMger.connectTo(elt4, elt3);
+    graphMger.connectTo(elt5, elt1);
+    graphMger.connectTo(elt5, elt3);
 
     QTimer::singleShot(waitMilliSec,this,&MainWindow::screenShot);
 }
@@ -24,10 +68,19 @@ MainWindow::~MainWindow()
 void MainWindow::screenShot()
 {
     BackgroundImage bi = m_screenShot.takeScreenshot();
+    BackgroundImage::box b;
+    QImage image;
+    for (GraphManager::Elt* elt : graphMger.getCurrentElt()->_outputElts)
+    {
+        image = elt->_pattern;
+        b = bi.findSampleImage(image,100);
+        if(b.isValid())
+        {
+            graphMger.setCurrentElt(elt);
+            break;
+        }
+    }
 
-    QImage image("D:/important/factures/tmp/test.png");
-
-    BackgroundImage::box b = bi.findSampleImage(image,10);
 
     if (b.isValid())
     {
@@ -51,6 +104,7 @@ void MainWindow::screenShot()
         }
     }
 
+    ui->pattern->setPixmap(QPixmap::fromImage(image));
     ui->image->setPixmap(QPixmap::fromImage(bi).scaled(bi.width()/2,bi.height()/2));
     QTimer::singleShot(waitMilliSec,this,&MainWindow::screenShot);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,8 +4,8 @@
 #include <QMainWindow>
 #include <screenshot.h>
 #include <backgroundimage.h>
+#include <graphmanager.h>
 #include <QImage>
-
 
 namespace Ui {
 class MainWindow;
@@ -26,6 +26,7 @@ private:
     Ui::MainWindow *ui;
     Screenshot m_screenShot;
     QImage image;
+    GraphManager graphMger;
 };
 
 #endif // MAINWINDOW_H
